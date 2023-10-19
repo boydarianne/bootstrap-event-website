c878ee85742a6943c77c79cef6159145b4df8ed0
Arianne Boyd <boydarianne@yahoo.com>
2023-10-18 21:41:58 -0400

    added javascript to js file and using bootstrap to about.html

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 'use strict';
 
 console.log ("hello world")
+
+
+function getFirstName(event) {
+event.preventDefault();
+
+const value = event.target.value;
+
+console.log(value)
+console.log(event)
+}
+
+
+
+function getLastName(event) {
+    event.preventDefault();
+    
+    const value = event.target.value;
+    
+    console.log(value)
+    }
