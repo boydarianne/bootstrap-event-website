5c371d4503a7367eb88063cebbcfb6efaa43d487
Arianne Boyd <boydarianne@yahoo.com>
2023-10-25 21:46:47 -0400

    created constructor function for events

--- app.js
+++ app.js
@@ -21,77 +21,70 @@ function getLastName(event) {
   console.log(value);
 }
 
-//object literal for event name, date, and time
+//event countdown timer using object literal
+const eventObject = {
+  eventName: "Art Explosion 2023",
+  eventDateAndTime: new Date("2023-10-31T13:30:00").getTime(),
 
-// const artEvents = () => {
-    
-//     eventName: "art explosion 2023",
-//     const eventDate: new Date("2023-10-31T13:30:00").getTime(),
-//     today:  new Date ().getTime(),
-//     timeLapse = eventDate - today;
-  
+  timerLapse: function () {
+    const now = new Date().getTime();
+    const distance = this.eventDateAndTime - now;
 
-//   //properties
-//   days: null,
-//   hours: null,
-//   minutes: null,
-//   seconds: null,
-//   //method
-//   timerSet: function () {
-// //     setInterval(() => {
-// //       //do the math to update days, hours, minutes, and seconds
-// //     let timeLapse = this.eventDate - this.today;
-// //       //to do: write render method and call it here
-// //       //   console.log(this.days, this.hours, this.minutes, this.seconds);
-// //     }, 1000);
-// //   },
-//   };
+    if (distance < 0) {
+      clearInterval(this.setTimer);
+      document.getElementById("timer").innerHTML = "Event has started!";
+      return;
+    }
 
-// setInterval(,1000);
-// //calculate and display a countdown timer that shows the time remaining until the event starts
+    // Calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+    // Update the HTML elements with the calculated values
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+  },
 
-//Object Literal for event name, date, and time
-// const eventObject = {
-//     eventName:"Art Explosion 2023",
-//     eventDateAndTime: new Date("2023-10-31T13:30:00").getTime(),
+  // Start the timer when the object is created
+  setTimer: setInterval(function () {
+    eventObject.timerLapse();
+  }, 1000),
+};
 
-    // timerLapse: function (){
-    //     setInterval(() => { 
-    //     },1000);
-    // },
+//constructor function for events
+function EventDetails(name, date, time, location) {
+  this.name = name;
+  this.date = date;
+  this.time = time;
+  this.location = location;
+}
+//constructor function for biography
+function Bio() {
+  this.Bio = [];
+}
 
-    const eventObject = {
-        eventName: "Art Explosion 2023",
-        eventDateAndTime: new Date("2023-10-31T13:30:00").getTime(),
-       
-        timerLapse: function (){
-            const now = new Date().getTime();
-            const distance = this.eventDateAndTime - now;
-           
-            if(distance < 0) {
-                clearInterval(this.setTimer);
-                document.getElementById("timer").innerHTML = "Event has started!";
-                return;
-            }
-    
-            // Calculations for days, hours, minutes and seconds
-            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-           
-            // Update the HTML elements with the calculated values
-            document.getElementById("days").innerText = days;
-            document.getElementById("hours").innerText = hours;
-            document.getElementById("minutes").innerText = minutes;
-            document.getElementById("seconds").innerText = seconds;
-        },
-       
-        // Start the timer when the object is created
-        setTimer: setInterval(function() {
-            eventObject.timerLapse();
-        }, 1000)
-    };
+const artExplosion = new EventDetails(
+  "Art Explosion",
+  "2023-10-31",
+  "13:30:00",
+  "superdome"
+);
+const colorJam = new EventDetails(
+    "Color Jam",
+    "2023-11-11",
+    "12:00:00",
+    "smoothie king center"
+)
+const artBasel = new EventDetails(
+    "Art Basel",
+    "2024-12-08",
+    "11:15:00",
+    "Miami Beach Convention Center"
+)
 
-    
